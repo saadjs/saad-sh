b2f59665dafa3aec052002d6feea06f3fac531c9
Saad Bash <skbash89@gmail.com>
2026-04-03 19:50:56 -0400

    fix: escape rss feed xml

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -3,6 +3,19 @@ import { siteConfig } from "@/site.config";
 
 export const revalidate = 3600;
 
+function escapeXml(value: string) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+function wrapCdata(value: string) {
+  return `<![CDATA[${value.replaceAll("]]>", "]]]]><![CDATA[>")}]]>`;
+}
+
 export async function GET() {
   const posts = await getAllPosts();
   const latestPostDate = posts[0] ? new Date(posts[0].metadata.date) : new Date(0);
@@ -11,13 +24,13 @@ export async function GET() {
     .map(
       (post) => `
     <item>
-      <title><![CDATA[${post.metadata.title}]]></title>
-      <link>${siteConfig.url}${siteConfig.routes.posts}/${post.slug}</link>
-      <guid isPermaLink="true">${siteConfig.url}${siteConfig.routes.posts}/${post.slug}</guid>
-      <description><![CDATA[${post.metadata.description}]]></description>
+      <title>${wrapCdata(post.metadata.title)}</title>
+      <link>${escapeXml(`${siteConfig.url}${siteConfig.routes.posts}/${post.slug}`)}</link>
+      <guid isPermaLink="true">${escapeXml(`${siteConfig.url}${siteConfig.routes.posts}/${post.slug}`)}</guid>
+      <description>${wrapCdata(post.metadata.description)}</description>
       <pubDate>${new Date(post.metadata.date).toUTCString()}</pubDate>
-      <dc:creator><![CDATA[${siteConfig.author.name}]]></dc:creator>
-      ${post.metadata.tags.map((tag) => `<category><![CDATA[${tag}]]></category>`).join("")}
+      <dc:creator>${wrapCdata(siteConfig.author.name)}</dc:creator>
+      ${post.metadata.tags.map((tag) => `<category>${wrapCdata(tag)}</category>`).join("")}
     </item>`,
     )
     .join("");
@@ -25,20 +38,20 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
   <channel>
-    <title>${siteConfig.name}</title>
-    <link>${siteConfig.url}</link>
-    <description>${siteConfig.description}</description>
-    <language>${siteConfig.language}</language>
+    <title>${escapeXml(siteConfig.name)}</title>
+    <link>${escapeXml(siteConfig.url)}</link>
+    <description>${escapeXml(siteConfig.description)}</description>
+    <language>${escapeXml(siteConfig.language)}</language>
     <lastBuildDate>${latestPostDate.toUTCString()}</lastBuildDate>
-    <atom:link href="${siteConfig.url}${siteConfig.routes.feed}" rel="self" type="application/rss+xml"/>
-    <generator>${siteConfig.name}</generator>
+    <atom:link href="${escapeXml(`${siteConfig.url}${siteConfig.routes.feed}`)}" rel="self" type="application/rss+xml"/>
+    <generator>${escapeXml(siteConfig.name)}</generator>
     ${itemsXml}
   </channel>
 </rss>`;
 
   return new Response(xml, {
     headers: {
-      "Content-Type": "application/xml",
+      "Content-Type": "application/rss+xml; charset=utf-8",
     },
   });
 }
